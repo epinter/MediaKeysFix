3f8cd2536d8900c42882165639719656614d72cc
Emerson Pinter <e@pinter.dev>
2024-01-23 16:21:58 -0300

    Update copyright/license notices

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,15 +1,7 @@
 /*
 Copyright (c) 2023 Emerson Pinter
 
-This file is part of Media Keys Fix SKSE.
-
-Foobar is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
-by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
-Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+This file is part of Media Keys Fix. Licensed under LGPL-3.0-or-later <https://www.gnu.org/licenses/lgpl-3.0.txt>.
 */
 
 #include "Main.h"
